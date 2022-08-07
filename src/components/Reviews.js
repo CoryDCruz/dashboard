d64e4146861bcfebdf58c01565e8f42a069c361d
Cory Dela Cruz <corydcruz@gmail.com>
2022-08-06 17:35:10 -0700

    sidebar complete working on reviews component

---
+++ src/components/Reviews.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "@mui/system";
+import { Card, Typography } from "@mui/material";
+import { CardHeader } from "@mui/material";
+import { CardContent } from "@mui/material";
+
+export default function Reviews() {
+return(
+    <Card sx={{ mxWidth: 200 }}>
+        <CardHeader title="Reviews"/>
+    </Card>
+    
+
+)
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { Typography } from '@mui/material';
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <List>
+          {['Dashboard', 'Widget', 'Reviews', 'Customers', 'Online Analysis', 'Settings'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
